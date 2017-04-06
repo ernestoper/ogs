1213ba63a4a2034d770498ba4800b595337fdffd
Wenqing Wang <wenqing.wang@ufz.de>
2017-04-06 18:20:49 +0200

    [dt] Added a test for EvolutionaryPIDcontroller

--- Tests/NumLib/TestTimeSteppingEvolutionaryPIDcontroller.cpp
+++ Tests/NumLib/TestTimeSteppingEvolutionaryPIDcontroller.cpp
@@ -50,10 +50,10 @@ TEST(NumLibTimeStepping, testEvolutionaryPIDcontroller)
         "<time_stepping>"
         "   <type>EvolutionaryPIDcontroller</type>"
         "   <t_initial> 0.0 </t_initial>"
-        "   <t_end> 300 </t_end>"
+        "   <t_end> 10 </t_end>"
         "   <dt_guess> 0.01 </dt_guess>"
         "   <dt_min> 0.001 </dt_min>"
-        "   <dt_max> 10 </dt_max>"
+        "   <dt_max> 1 </dt_max>"
         "   <rel_dt_min> 0.01 </rel_dt_min>"
         "   <rel_dt_max> 5 </rel_dt_max>"
         "   <tol> 1.e-3 </tol>"
@@ -65,33 +65,57 @@ TEST(NumLibTimeStepping, testEvolutionaryPIDcontroller)
     // 1st step
     ASSERT_TRUE(PIDSteper->next(solution_error));
     NumLib::TimeStep ts = PIDSteper->getTimeStep();
+    double h_new = 0.01;
+    double t_previous = 0.;
     ASSERT_EQ(1u, ts.steps());
-    ASSERT_EQ(0., ts.previous());
-    ASSERT_EQ(0.01, ts.current());
-    ASSERT_EQ(0.01, ts.dt());
+    ASSERT_EQ(t_previous, ts.previous());
+    ASSERT_EQ(t_previous + h_new, ts.current());
+    ASSERT_EQ(h_new, ts.dt());
     ASSERT_TRUE(PIDSteper->accepted());
+    t_previous += h_new;
 
     // e_n_minus1 is filled.
     solution_error = 1.0e-4;
     PIDSteper->next(solution_error);
     ts = PIDSteper->getTimeStep();
+    h_new = ts.dt();
     ASSERT_EQ(2u, ts.steps());
-    ASSERT_EQ(0.01, ts.previous());
-    ASSERT_NEAR(0.024962356560944334, ts.current(), 1.e-10);
-    ASSERT_NEAR(0.014962356560944334, ts.dt(), 1.e-10);
+    ASSERT_NEAR(t_previous, ts.previous(), 1.e-10);
+    ASSERT_NEAR(t_previous + h_new, ts.current(), 1.e-10);
     ASSERT_TRUE(PIDSteper->accepted());
+    t_previous += h_new;
 
     // e_n_minus2 is filled.
     solution_error = 0.5e-3;
     PIDSteper->next(solution_error);
     ts = PIDSteper->getTimeStep();
+    h_new = ts.dt();
     ASSERT_EQ(3u, ts.steps());
-    ASSERT_NEAR(0.024962356560944334, ts.previous(), 1.e-10);
-    ASSERT_NEAR(0.039933587130577874, ts.current(), 1.e-10);
-    ASSERT_NEAR(0.01497123056963354, ts.dt(), 1.e-10);
+    ASSERT_NEAR(t_previous, ts.previous(), 1.e-10);
+    ASSERT_NEAR(t_previous + h_new, ts.current(), 1.e-10);
     ASSERT_TRUE(PIDSteper->accepted());
 
-    double tc = ts.current();
-    double tp = ts.previous();
-    double dt = ts.dt();
+    // error > TOL=1.3-3, step rejected and a new step size estimated.
+    solution_error = 0.01;
+    PIDSteper->next(solution_error);
+    ts = PIDSteper->getTimeStep();
+    h_new = ts.dt();
+    // No change in ts.steps
+    ASSERT_EQ(3u, ts.steps());
+    // No change in ts.previous(), which is the same as that of the previous
+    // step.
+    ASSERT_NEAR(t_previous, ts.previous(), 1.e-10);
+    ASSERT_NEAR(t_previous + h_new, ts.current(), 1.e-10);
+    ASSERT_FALSE(PIDSteper->accepted());
+    t_previous += h_new;
+
+    // With e_n, e_n_minus1, e_n_minus2
+    solution_error = 0.4e-3;
+    PIDSteper->next(solution_error);
+    ts = PIDSteper->getTimeStep();
+    h_new = ts.dt();
+    ASSERT_EQ(4u, ts.steps());
+    ASSERT_NEAR(t_previous, ts.previous(), 1.e-10);
+    ASSERT_NEAR(t_previous + h_new, ts.current(), 1.e-10);
+    ASSERT_TRUE(PIDSteper->accepted());
 }
